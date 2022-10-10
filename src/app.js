aaa58c7fbbf2d42aa30371f060d08fd90560f4aa
Sebastián Saldaña <ssaldanacardenas@gmail.com>
2022-10-10 16:56:25 -0500

    Adds basic table geometry

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import {
     BoxGeometry,
+    CameraHelper,
     Color,
+    CylinderGeometry,
     Mesh,
     MeshBasicMaterial,
+    MeshDepthMaterial,
     PerspectiveCamera, 
     Scene, 
     WebGLRenderer, } from 'three';
@@ -16,28 +19,41 @@ function main() {
         const scene = new Scene();
         const sceneBackgroundColor = new Color( 0xFFFFFF );
         scene.background = sceneBackgroundColor;
-        const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
         const renderer = new WebGLRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
         document.body.appendChild( renderer.domElement );
+        
+        // candidate for refactoring into another function outside the main function
+        const leg = (xpos, ypos, zpos) => {
+            const legMaterial = new MeshBasicMaterial({color: 'black'});
+            const legGeometry = new CylinderGeometry(1, 1, 8, 32)
+            const legMesh = new Mesh(legGeometry, legMaterial);
+            legMesh.position.x = xpos;
+            legMesh.position.y = ypos;
+            legMesh.position.z = zpos;
+            return legMesh;
+        }
 
-        const geometry = new BoxGeometry( 1, 1, 1 );
-        const material = new MeshBasicMaterial( { color: 0xF25278 } );
-        const cube = new Mesh( geometry, material );
-        scene.add( cube );
+        const tableTopGeometry = new BoxGeometry(9.5, 0.75, 7);
+        const tableTopMaterial = new MeshBasicMaterial({color: 'black'})
+        const tableTopMesh = new Mesh(tableTopGeometry, tableTopMaterial);
+        tableTopMesh.position.x = 4;
+        tableTopMesh.position.z = 0;
+        tableTopMesh.position.y = 6;
 
-        camera.position.z = 5;
+        scene.add(leg(0, 0, 0));
+        scene.add(leg(8, 0, 0));
+        scene.add(leg(0, 0, -8));
+        scene.add(leg(8, 0, -8));
+        scene.add(tableTopMesh);
 
-        function animate() {
-            requestAnimationFrame( animate );
-
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
-
-            renderer.render( scene, camera );
-        };
-
-        animate();
+        camera.position.z = 35;
+        camera.position.y = 20;
+        camera.position.x = 10;
+        const helper = new CameraHelper( camera );
+        scene.add( helper );
+        renderer.render( scene, camera );
     } else {
         const warning = WebGL.getWebGLErrorMessage();
 	    document.getElementById( 'container' ).appendChild( warning );
