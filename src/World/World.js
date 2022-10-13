379478ae0e73df2415add2f666d749a869d93487
David Hernández <d.hdza19@gmail.com>
2022-10-13 18:51:08 -0500

    Add audio and resizer

--- src/World/World.js
+++ src/World/World.js
@@ -6,17 +6,28 @@ import { createDisk } from './components/disk.js';
 
 import { createRenderer } from './systems/renderer.js';
 import { Resizer } from './systems/Resizer.js';
+import { createAudio } from './systems/audio.js';
+import { createListener } from './systems/listener.js';
+
+import { playAudio } from './actions/playAudio.js'
 
 let scene;
 let camera;
 let renderer;
+let listener;
+let audio;
 
 class World {
   constructor(container) {
     camera = createCamera();
     scene = createScene();
     renderer = createRenderer();
-    container.append(renderer.domElement);
+    listener = createListener();
+    audio = createAudio( listener );
+    camera.add( listener ); // for audio to play
+    playAudio( audio );
+    
+    container.append( renderer.domElement );
 
     const table = createTable();
     const disk = createDisk();
@@ -25,6 +36,9 @@ class World {
     scene.add(disk, table, light);
 
     const resizer = new Resizer(container, camera, renderer);
+    resizer.onResize = () => {
+      this.render();
+    };
   }
 
   render() {
